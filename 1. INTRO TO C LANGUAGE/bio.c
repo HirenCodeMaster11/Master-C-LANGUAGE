4b664f4f5150a9f5446411028424893789d88179
Hiren Bambhaniya <148859956+hirenbambhaniya@users.noreply.github.com>
2023-10-31 09:55:01 +0530

    Add files via upload

---
+++ 1. INTRO TO C LANGUAGE/bio.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+	printf("name : Hren Bambhaniya\n");
+	printf("age : 18\n");
+	printf("school : Gyan Jyot Vidyalaya school\n");
+	printf("hobby : gaming ");
+}
---
+++ 1. INTRO TO C LANGUAGE/name 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	printf("------\n");
+	printf("|    |\n");
+	printf("H    |\n");
+	printf("I    |\n");
+	printf("R    |\n");
+	printf("E    |\n");
+	printf("N    |\n");
+	printf("|    |\n");
+	printf("------\n");
+}
