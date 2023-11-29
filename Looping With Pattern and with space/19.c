7bc105b246f532bcb5f1dc18acb3bade08cf5c9b
Hiren Bambhaniya <148859956+HirenCodeMaster11@users.noreply.github.com>
2023-11-29 22:02:06 +0530

    Add files via upload

---
+++ Looping With Pattern and with space/19.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+main()
+{
+	char i,j,k;
+	
+	for(i=65; i<=69; i++)
+	{
+		for(k=65; k<i; k++)
+		{
+			printf(" ");
+		}
+		for(j=i; j<=69; j++)
+		{
+			if(j%2==0)
+			{
+				printf("0");	
+			}	
+			else
+			{
+				printf("1");
+			}
+		}
+		printf("\n");
+	}
+}
