013137ee0d8004f7eba2492e7e5a05a05b537ce1
Hiren Bambhaniya <148859956+HirenCodeMaster11@users.noreply.github.com>
2023-11-30 20:19:59 +0530

    Add files via upload

---
+++ LOOPING WITH PATTERNS/CUSTOM PATTERN ABCD/cus V.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j;
+	
+	for(i=1; i<=6; i++)
+	{
+		for(j=1; j<=5; j++)
+		{
+			if(j==1 && i!=5 && i!=6 || j==5 && i!=5 && i!=6 || i==5 && j==2 || i==6 && j==3|| i==5 && j==4)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+}
